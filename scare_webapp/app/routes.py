ac9716df3f88080c709468159450dee3e8a4d5c3
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2020-09-19 21:51:35 +0200

    Remove unnecessary code

--- scare_webapp/app/routes.py
+++ scare_webapp/app/routes.py
@@ -2,7 +2,7 @@ from app import app
 from flask import render_template, flash, redirect, url_for, request
 from app.models import Tiles, Tile_density
 from app import db
-from sqlalchemy import select
+#from sqlalchemy import select
 
 @app.route('/')
 @app.route('/index')
@@ -12,23 +12,6 @@ def index():
     # .outerjoin(Tiles, Tiles.tileId == Tile_density.tileId) \
     # .all()
 
-    density = db.session.query(Tiles, Tile_density).join(Tile_density, Tiles.tileId == Tile_density.tileId).all()
-    density = [[float(each[0].llx), float(each[0].lly), float(each[1].density)] for each in density]
-    return render_template("index.html", title='Corona Scare Application', density_data=density)
-
-@app.route('/scaremap')
-def scaremap():
-    # density = Tile_density.query \
-    # .filter(Tile_density.maleProportion>0) \
-    # .outerjoin(Tiles, Tiles.tileId == Tile_density.tileId) \
-    # .all()
-
-    density = db.session.query(Tiles, Tile_density).join(Tile_density, Tiles.tileId == Tile_density.tileId).all()
-
-    density = [{
-        "llx" : each[0].llx,
-        "lly" : each[0].lly,
-        "date" : each[1].tileDate,
-        "value" : each[1].density
-    } for each in density]
-    return render_template("scaremap.html", density_data=density)
+    #density = db.session.query(Tiles, Tile_density).join(Tile_density, Tiles.tileId == Tile_density.tileId).all()
+    #density = [[float(each[0].llx), float(each[0].lly), float(each[1].density)] for each in density]
+    return render_template("index.html", title='Corona Scare Application') #, density_data=density)
