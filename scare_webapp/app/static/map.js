aa5cd7ad4262f176054dd09d9b347000e9d4a42e
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2020-09-19 21:33:46 +0200

    Remove unnecessary code

--- scare_webapp/app/static/map.js
+++ scare_webapp/app/static/map.js
@@ -39,30 +39,9 @@ var body = d3.select("body"),
         .append("div")
         .attr("class", "ttip hidden");
 
-// Define the zoom and attach it to the map
-var zoom = d3.behavior.zoom()
-    .scaleExtent([1, 10])
-    .on('zoom', doZoom);
-map.call(zoom);
-
 // Define the projection of the map (center and scale will be defined
 // later)
 var proj = d3.geo.mercator();
-
-// const density_data = JSON.parse(document.currentScript.getAttribute('density_data'));//.slice(0, 100000);
-//
-// map
-//     .selectAll("myCircles")
-//     .data(density_data)
-//     .enter()
-//     .append("circle")
-//     .attr("cx", function(d){ return proj([d[0], d[1]])[0] })
-//     .attr("cy", function(d){ return proj([d[0], d[1]])[1] })
-//     .attr("r", 14)
-//     .style("fill", "69b3a2")
-//     .attr("stroke", "#69b3a2")
-//     .attr("stroke-width", 3)
-//     .attr("fill-opacity", .4)
 
 // Prepare the cartogram
 var topology,
@@ -84,28 +63,9 @@ var fields = [
       {name: "Number of tweets", id: "tweets_total", key: "%d"}
     ],
     months = [20, 21, 22, 23, 24, 25], //["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"],
-    fieldsById = d3.nest()
-        .key(function(d) { return d.id; })
-        .rollup(function(d) { return d[0]; })
-        .map(fields),
-    field = fields[0],
     month = months[0],
     currentKey;
 
-// Define the dropdown to select a field to scale by.
-var fieldSelect = d3.select("#field")
-    .on("change", function(e) {
-      // On change, update the URL hash
-      field = fields[this.selectedIndex];
-      location.hash = "#" + [field.id, month].join("/");
-    });
-// Populate its options with the fields available
-fieldSelect.selectAll("option")
-    .data(fields)
-    .enter()
-    .append("option")
-    .attr("value", function(d) { return d.id; })
-    .text(function(d) { return d.name; });
 
 // Define the dropdown to select a month.
 var monthSelect = d3.select("#year");
@@ -115,8 +75,15 @@ var monthSelect = d3.select("#year");
 $('#year').on("change", function(e) {
   // On change, update the URL hash
   month = months[this.selectedIndex];
-  location.hash = "#" + [field.id, month].join("/");
 });
+
+d3.select("#keywords")
+  .on("change", function(e) {
+  // On change, update the URL hash
+  keywords = this.value;
+  update();
+});
+
 
 // Populate its options with the months available
 monthSelect.selectAll("option")
@@ -126,57 +93,10 @@ monthSelect.selectAll("option")
     .attr("value", function(y) { return y; })
     .text(function(y) { return y; })
 
-// Define some variables needed for the play functionality
-var isPlaying,
-    outerIsPlaying, innerIsPlaying;
-
-// Define what happens when the play buttons are clicked
-var playButton = $('#play');
-var stopButton = $('#stop');
-playButton.on('click', function() {
-  stop_play();
-  playButton.prop('disabled', true);
-  stopButton.prop('disabled', false);
-  isPlaying = true;
-  play_inner();
-})
-stopButton.on('click', function() {
-  playButton.prop('disabled', false);
-  stopButton.prop('disabled', true);
-  stop_play();
-});
-
-function stop_play() {
-  isPlaying = false;
-  clearTimeout(innerIsPlaying);
-  clearTimeout(outerIsPlaying);
-}
-
-/**
- * Recursive function to loop infinitively through the fields in the
- * month dropdown.
- *
- * Caution: This function has no exit, it does not stop by itself!
- */
-function play_inner() {
-  $('#year>option').each(function(i) {
-    var el = $(this);
-    innerIsPlaying = setTimeout(function() {
-      if (isPlaying) {
-        // Trigger a change event
-        el.prop("selected", true).change();
-      }
-    }, i * 2000);
-  });
-  outerIsPlaying = setTimeout(function() {
-    play_inner();
-  }, months.length * 2000);
-}
-
-// Add a listener to the change of the URL hash
-window.onhashchange = function() {
-  parseHash();
-};
+// // Add a listener to the change of the URL hash
+// window.onhashchange = function() {
+//   parseHash();
+// };
 
 // Read the geometry data
 d3.json("static/ch_cantons.topojson", function(topo) {
@@ -243,32 +163,14 @@ function init() {
   mapFeatures.on('mousemove', showTooltip)
       .on('mouseout', hideTooltip);
 
-  // Parse the URL hash to see if the map was loaded with a cartogram
-  parseHash();
-}
-
-/**
- * Reset the cartogram and show the features without scaling.
- */
-function reset() {
-  // Create the cartogram features (without any scaling)
-  var cartoFeatures = carto.features(topology, geometries),
-      path = d3.geo.path()
-          .projection(proj);
-
-  // Redraw the features with a transition
-  mapFeatures.data(cartoFeatures)
-      .transition()
-      .duration(750)
-      .ease("linear")
-      .attr("fill", "#ddd")
-      .attr("d", path);
+  // // Parse the URL hash to see if the map was loaded with a cartogram
+  // parseHash();
 }
 
 function update() {
 
   // Update the current key
-  currentKey = field.key.replace("%d", month);
+  currentKey = month;
 
   // display Tweets
   d3.json("static/twitter.json", function(twitter) {
@@ -276,7 +178,9 @@ function update() {
     // console.log(new Date(twitter[0].date).getDate())
     //twitter = twitter.filter(tweet => new Date(tweet.date).getMinutes() === parseInt(currentKey))
     //twitter = twitter.filter(tweet => tweet.score < -0.8);
-    //console.log(twitter)
+    console.log(keywords)
+    twitter = twitter.filter(tweet => keywords.split(',').some(keyword => tweet.text.toLowerCase().indexOf(keyword.toLowerCase()) !== -1));
+    console.log(twitter)
     // map
     //     .selectAll("myCircles")
     //     .remove();
@@ -313,13 +217,6 @@ function update() {
       lo = values[0],
       hi = values[values.length - 1];
 
-  // Determine the colors within the range
-  var color = d3.scale.linear()
-      .range(colors)
-      .domain(lo < 0
-          ? [lo, 0, hi]
-          : [lo, d3.mean(values), hi]);
-
   // Normalize the scale to positive numbers
   var scale = d3.scale.linear()
       .domain([lo, hi])
@@ -333,86 +230,7 @@ function update() {
   // Generate the new features and add them to the map
   var cartoFeatures = carto(topology, geometries).features;
   mapFeatures.data(cartoFeatures);
-
-  // Scale the cartogram with a transition
-  mapFeatures.transition()
-      .duration(750)
-      .ease("linear")
-      .attr("fill", function(d) {
-        return color(value(d));
-      })
-      .attr("d", carto.path);
-
-  hideUpdateIndicator();
 }
-
-
-/**
- * Use a deferred function to update the cartogram. This allows to set a
- * timeout limit.
- */
-var deferredUpdate = (function() {
-  var timeout;
-  return function() {
-    clearTimeout(timeout);
-    return timeout = setTimeout(function() {
-      update.apply(null, arguments);
-    }, 10);
-  };
-})();
-
-
-/**
- * Parse the URL hash location to determine whether to show features
- * scaled or not.
- */
-function parseHash() {
-
-  // Extract the field and month from the URL hash
-  var parts = location.hash.substr(1).split("/"),
-      desiredFieldId = parts[0],
-      desiredmonth = +parts[1];
-
-  // Make sure field and month are valid, else use the defaults
-  field = fieldsById[desiredFieldId] || fields[0];
-  month = (months.indexOf(desiredmonth) > -1) ? desiredmonth : months[0];
-
-  // Mark the field as selected in the dropdown
-  fieldSelect.property("selectedIndex", fields.indexOf(field));
-
-  if (field.id === "none") {
-    // If no scale is used, disable the month dropdown and the play
-    // button
-    monthSelect.attr("disabled", "disabled");
-    playButton.prop('disabled', true);
-    reset();
-  } else {
-    if (!isPlaying) {
-      playButton.prop('disabled', false);
-    }
-    // If month is selected, mark it as selected in the dropdown
-    if (field.months) {
-      if (field.months.indexOf(month) === -1) {
-        month = field.months[0];
-      }
-      monthSelect.selectAll("option")
-          .attr("disabled", function(y) {
-            return (field.months.indexOf(y) === -1) ? "disabled" : null;
-          });
-    } else {
-      monthSelect.selectAll("option")
-          .attr("disabled", null);
-    }
-
-    monthSelect
-        .property("selectedIndex", months.indexOf(month))
-        .attr("disabled", null);
-
-    deferredUpdate();
-    location.replace("#" + [field.id, month].join("/"));
-  }
-}
-
 
 /**
  * Show a tooltip with details of a feature, e.g. when hovering over a
@@ -453,35 +271,6 @@ function showTooltip(d, i) {
  */
 function hideTooltip() {
   tooltip.classed("hidden", true);
-}
-
-
-/**
- * Zoom the features on the map.
- */
-function doZoom() {
-
-  // Zoom and keep the stroke width proportional
-  mapFeatures.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")").style("stroke-width", .5 / d3.event.scale + "px");
-
-  // Hide the tooltip after zooming
-  hideTooltip();
-}
-
-
-/**
- * Show an update indicating while calculating the cartogram.
- */
-function showUpdateIndicator() {
-  body.classed("updating", true);
-}
-
-
-/*
- * Hide the update indicator after the cartogram was calculated.
- */
-function hideUpdateIndicator() {
-  body.classed("updating", false);
 }
 
 
